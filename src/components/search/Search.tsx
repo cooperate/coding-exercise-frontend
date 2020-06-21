28d65ef8b9cf61c4d936c53f459f399bde9d47ff
cooperate1 <brett.cizmar@hotmail.com>
2020-06-21 12:29:42 -0700

    Cleanup Search Component.

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,39 +2,9 @@ import React from "react";
 import Input from "../input/Input";
 import InputAdornment from '@material-ui/core/InputAdornment';
 import SearchIcon from "@material-ui/icons/Search";
-import { fade, makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
 
 const Search = (props: any) => {
   const {updateSearchTerm} = props;
-  const classes = useStyles();
   const handleChange = (e: any) => {
     updateSearchTerm(e.target.value);
   }
