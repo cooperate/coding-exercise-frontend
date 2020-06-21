901cd2ebe89a8a3c1919c444523561281f2fc791
cooperate1 <brett.cizmar@hotmail.com>
2020-06-21 12:30:07 -0700

    Add spec for Search.

---
+++ src/components/search/Search.spec.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow, configure } from "enzyme";
+import { expect } from "chai";
+import Search from "./Search";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+describe("<Search />", () => {
+
+});
